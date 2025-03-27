89186a9a4f576aea4ce4278901de8287d1447bb0
tyctw <zhangwe886@gmail.com>
2025-03-27 22:47:56 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ export const config = {
   SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbw-UIFq9rqjW0v3dS9k1dTgx7Egh5xQnbqcamt5zyvkQLJqk7DLFMiWdvrwEmfd7kTVdA/exec',
   
   // hCaptcha settings
-  HCAPTCHA_SITE_KEY: '10000000-ffff-ffff-ffff-000000000001', // Replace with your actual site key
+  HCAPTCHA_SITE_KEY: 'ceced506-fc3a-4214-976e-9514bf1eea48', // Replace with your actual site key
   
   // Security settings
   ENABLE_COPY_PROTECTION: false, // Changed to false to allow copy functionality
